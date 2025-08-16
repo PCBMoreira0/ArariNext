f4fe00dbe4bae320b09b62af2aa49ac2a011cf62
PCBMoreira0 <151090351+PCBMoreira0@users.noreply.github.com>
2025-08-16 20:41:49 -0300

    update some domain models

--- lib/domain/models/mppt_state_data.dart
+++ lib/domain/models/mppt_state_data.dart
@@ -1,8 +1,27 @@
 import 'package:arari_next/domain/models/iboat_data.dart';
 
+enum MpptBatteryStatus {
+  normal,
+  overvoltage,
+  undervoltage,
+  lowVoltageDisconnect,
+  fault,
+  unknown
+}
+
 final class MPPTStateData implements IBoatData {
   final int batteryStatus;
   final int chargingEquipmentStatus;
 
   MPPTStateData({required this.batteryStatus, required this.chargingEquipmentStatus});
+
+
+  // MpptBatteryStatus _getStatus(int status){
+  //   switch (status & 15) {
+  //     case 0:
+  //       return MpptBatteryStatus.normal;
+  //     case 
+  //     default:
+  //   }
+  // }
 }
